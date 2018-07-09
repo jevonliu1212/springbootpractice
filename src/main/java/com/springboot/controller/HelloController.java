c30c1bcee98c027ba74a240a139888d6890d0637
liujianwei <liujianwei@yazuo.com>
2018-07-09 17:08:30 +0800

    配置文件学习

--- src/main/java/com/springboot/controller/HelloController.java
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${info.name}")
+	private String name;
+	
 	@RequestMapping("/")
 	public String helloWord(){
-		return "Hello World";
+		return "Hello World,"+name;
 	}
 }
