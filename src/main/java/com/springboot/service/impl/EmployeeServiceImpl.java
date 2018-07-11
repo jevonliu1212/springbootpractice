94b04b563e1c4ea4b06bbc519e18ffa3012144e3
liujianwei <liujianwei@yazuo.com>
2018-07-11 16:14:57 +0800

    事务

--- src/main/java/com/springboot/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,14 @@
 package com.springboot.service.impl;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.dao.EmployeeDao;
 import com.springboot.entity.Employee;
 import com.springboot.service.EmployeeService;
+//@Transactional
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 	@Autowired
@@ -16,9 +19,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return employeeDao.getById(id);
 	}
 
+	
 	@Override
 	public int add(Employee e) {
-		return employeeDao.add(e);
+		employeeDao.add(e);
+		return 0;
 	}
 
 }
