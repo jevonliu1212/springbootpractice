7cfb1e41bffec39f4f5bba49859f8411e72f56ad
liujianwei <liujianwei@yazuo.com>
2018-07-12 11:25:13 +0800

    redis缓存

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.springboot.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
