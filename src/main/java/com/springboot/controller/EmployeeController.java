8e80a3b629ca3bd680ca863db46b3482eaf0b446
liujianwei <245938612@qq.com>
2018-07-14 17:16:51 +0800

    logback

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.springboot.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import com.springboot.service.EmployeeService;
 @RestController
 @RequestMapping("/emp")
 public class EmployeeController {
+	private final static Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
 	@Autowired
 	private EmployeeService employeeService;
@@ -25,7 +28,8 @@ public class EmployeeController {
 	
 	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
 	public Employee getById(@PathVariable("id")long id){
-		return employeeService.getById(id);
+		logger.info("查询人员信息id={}",id);
+		return employeeMapper.get(id);
 	}
 	
 	@RequestMapping(value = "/add",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
