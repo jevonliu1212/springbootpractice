c12448752d7fcbeecc25ed0f62202aea16116823
liujianwei <245938612@qq.com>
2018-08-28 00:49:52 +0800

    update

--- src/main/java/com/springboot/controller/WebSocketController.java
+++ src/main/java/com/springboot/controller/WebSocketController.java
@@ -2,6 +2,7 @@ package com.springboot.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 import com.springboot.websocket.Message;
@@ -15,5 +16,11 @@ public class WebSocketController {
     public Response say(Message message) throws Exception {
         Thread.sleep(1000);
         return new Response("Welcome, " + message.getName() + "!");
+    }
+	
+	@SubscribeMapping("/topic/getResponse")
+    public Response sub() {
+		System.out.println("XXX用户订阅了我。。。");
+        return new Response("感谢你订阅了我。。。");
     }
 }
